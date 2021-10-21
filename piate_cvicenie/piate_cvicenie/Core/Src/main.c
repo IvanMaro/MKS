3c1e0be2e6db09a720b2ab85f3b295392255d899
filip-dobrocky <filip.dobrocky@gmail.com>
2021-10-21 09:57:52 +0200

    CV5_upraviť_"for na konci"

--- piate_cvicenie/piate_cvicenie/Core/Src/main.c
+++ piate_cvicenie/piate_cvicenie/Core/Src/main.c
@@ -43,6 +43,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_rx;
 
@@ -61,6 +63,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 int _write(int file, char const *buf, int n)
@@ -75,9 +78,6 @@ int _write(int file, char const *buf, int n)
  	LED1 ON|OFF
  	LED2 ON|OFF
  	STATUS
- 	READ adresa
- 	WRITE adresa hodnota
-	DUMP
  */
 
 
@@ -130,26 +130,66 @@ static void uart_process_command(char *cmd)
 		printf("\n");
 
 	}
+	else if (strcasecmp(token, "READ") == 0) {
+		token = strtok(NULL, " ");
+		uint16_t addr = atoi(token);
+		uint8_t value;
+		HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
 
+		printf("Adresa 0x%04X = 0x%02X\n",addr,value);
 
-}
-
-
-
-
-static void uart_byte_available(uint8_t c)
-{
-	static uint16_t cnt;
-	static char data[CMD_BUFFER_LEN];
-
-	if (cnt < CMD_BUFFER_LEN && c >= 32 && c <= 126)
-		data[cnt++] = c;
-	if ((c == '\n' || c == '\r') && cnt > 0) {
-		data[cnt] = '\0';
-		uart_process_command(data);
-		cnt = 0;
 	}
+
+	else if (strcasecmp(token, "WRITE") == 0) {
+		token = strtok(NULL, " ");
+		uint16_t addr = atoi(token);
+		token = strtok(NULL, " ");
+		uint8_t value = atoi(token);
+		HAL_I2C_Mem_Write(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
+
+		/* Check if the EEPROM is ready for a new operation */
+		while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT) {}
+
+		printf("OK\n");
+	}
+
+	else if (strcasecmp(token, "DUMP") == 0) {
+			uint8_t value[0xF];
+			HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, 0, I2C_MEMADD_SIZE_16BIT, value, 0xF, 1000);
+
+			uint16_t i = 0;
+
+			for(; i < 0x8; i++){
+				printf("%02X ", value[i]);
+			}
+			printf("\n");
+
+			for(; i <= 0xF; i++){
+				printf("%02X ", value[i]);
+			}
+			printf("\n");
+		}
+		else{
+			printf("Neznamy prikaz!\n");
+	 	}
+
 }
+
+
+
+	static void uart_byte_available(uint8_t c)
+	{
+		static uint16_t cnt;
+		static char data[CMD_BUFFER_LEN];
+
+		if (cnt < CMD_BUFFER_LEN && c >= 32 && c <= 126)
+			data[cnt++] = c;
+		if ((c == '\n' || c == '\r') && cnt > 0) {
+			data[cnt] = '\0';
+			uart_process_command(data);
+			cnt = 0;
+		}
+	}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -187,6 +227,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_UART_Receive_DMA(&huart2, uart_rx_buf, RX_BUFFER_LEN);
@@ -217,6 +258,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -244,6 +286,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
